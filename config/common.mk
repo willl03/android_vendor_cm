willl03/vendor_slim

Add cool panel color init.d script
Change-Id: I21b29da4a63741713496aa413f09f46d3b87a5cf

config/common.mk
@@ -61,6 +61,7 @@ PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/slim/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
     vendor/slim/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim \
+    vendor/slim/prebuilt/common/etc/init.d/95color:system/etc/init.d/95color \
     vendor/slim/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Embed SuperUser

willl03/android_vendor_cm

Conflicts:
	config/common.mk

config/common.mk
@@ -96,6 +96,7 @@ PRODUCT_COPY_FILES += \
 # userinit support
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim \
+    vendor/cm/prebuilt/common/etc/init.d/95color:system/etc/init.d/95color \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # CM-specific init file