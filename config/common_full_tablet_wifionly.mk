c80007de9d83031b91c3bbe94d191a73ca0ea69f
willl03 <wgangers@gmail.com>
2015-01-28 23:56:45 -0500

    Change ring/notif/alarm defaults for Slim sounds

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,8 +3,8 @@ $(call inherit-product, vendor/cm/config/common_full.mk)
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.notification_sound=Rhea.ogg \
+    ro.config.alarm_alert=Carbon.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -6,8 +6,8 @@ include vendor/cm/config/cm_audio.mk
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.notification_sound=Rhea.ogg \
+    ro.config.alarm_alert=Carbon.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
