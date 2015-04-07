willl0344/vendor_slim

Add a few Samsung media sounds

config/common.mk
@@ -71,6 +71,12 @@ PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/remount_fullext4:system/etc/init.d/remount_fullext4 \
     vendor/slim/prebuilt/common/etc/init.d/sqlite_optimize:system/etc/init.d/sqlite_optimize
 
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/media/audio/notifications/S_Flowers.ogg:system/media/audio/notifications/S_Flowers.ogg \
+    vendor/slim/prebuilt/common/media/audio/notifications/S_On_time.ogg:system/media/audio/notifications/S_On_time.ogg \
+    vendor/slim/prebuilt/common/media/audio/notifications/S_Whistle.ogg:system/media/audio/notifications/S_Whistle.ogg \
+    vendor/slim/prebuilt/common/media/audio/ringtones/Basic_bell.ogg:system/media/audio/ringtones/Basic_bell.ogg
+
 # Workaround for NovaLauncher zipalign fails
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk

willl03/android_vendor_cm

Conflicts:
	config/common.mk
Change-Id: I177cba9f451471e95d0d0bd036875d51aaa78c46

config/common.mk
@@ -123,6 +123,12 @@ PRODUCT_COPY_FILES += \
 # T-Mobile theme engine
 include vendor/cm/config/themes_common.mk
 
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/media/audio/notifications/S_Flowers.ogg:system/media/audio/notifications/S_Flowers.ogg \
+    vendor/cm/prebuilt/common/media/audio/notifications/S_On_time.ogg:system/media/audio/notifications/S_On_time.ogg \
+    vendor/cm/prebuilt/common/media/audio/notifications/S_Whistle.ogg:system/media/audio/notifications/S_Whistle.ogg \
+    vendor/cm/prebuilt/common/media/audio/ringtones/Basic_bell.ogg:system/media/audio/ringtones/Basic_bell.ogg
+
 # Required CM packages
 PRODUCT_PACKAGES += \
     Development \