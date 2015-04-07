willl03/vendor_slim

Add fstrim init.d script
Change-Id: I14cd48df8efebd20ce076863842438b68e5f4753

config/common.mk
@@ -60,6 +60,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/slim/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
+    vendor/slim/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim \
     vendor/slim/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Embed SuperUser

willl03/android_vendor_cm

Conflicts:
	config/common.mk

config/common.mk
@@ -95,6 +95,7 @@ PRODUCT_COPY_FILES += \
 
 # userinit support
 PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/init.d/99fstrim:system/etc/init.d/99fstrim \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
 # CM-specific init file